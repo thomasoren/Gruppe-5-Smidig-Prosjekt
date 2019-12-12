cbd0264f9f61a260a666849d3837f6c8fd936d4b
Thomas Øren <42614221+thomasoren@users.noreply.github.com>
2019-12-12 11:05:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/front/ui/shop/ShopFragment.java
@@ -0,0 +1,35 @@
+package com.example.front.ui.shop;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.front.R;
+
+public class ShopFragment extends Fragment {
+
+    private ShopViewModel shopViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        shopViewModel =
+                ViewModelProviders.of(this).get(ShopViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_shop, container, false);
+        final TextView textView = root.findViewById(R.id.text_shop);
+        shopViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/front/ui/time/TimeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.front.ui.time;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class TimeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public TimeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
